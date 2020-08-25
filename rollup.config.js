3a2c302ecafec45252610c6ff68de042dd27ac5f
Matt Travi <programmer@travi.org>
2020-08-24 23:14:50 -0500

    feat(bin): exposed the initial binary

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import autoExternal from 'rollup-plugin-auto-external';
+import json from 'rollup-plugin-json';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal()],
-  output: [
-    {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
-    {file: 'lib/index.es.js', format: 'es', sourcemap: true}
-  ]
+  plugins: [autoExternal(), json()],
+  output: [{file: 'bin/pragmatic-divops.js', format: 'cjs', sourcemap: true, banner: '#!/usr/bin/env node'}]
 };
