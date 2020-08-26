70d3a10886e142940b34194b96e6dea7f2a4e4f4
Matt Travi <programmer@travi.org>
2020-08-25 23:18:49 -0500

    wip(scaffold): added the enhanced scaffolders

--- src/commands/scaffold/enhanced-scaffolders.js
+++ src/commands/scaffold/enhanced-scaffolders.js
@@ -1,7 +1,26 @@
-export function javascriptScaffolderFactory() {
+import {scaffold as scaffoldJavaScript} from '@travi/javascript-scaffolder';
+import {scaffold as scaffoldGitHubActionsCi} from '@form8ion/github-actions-node-ci';
+import {scaffold as scaffoldMocha} from '@form8ion/mocha-scaffolder';
+import {prompt} from '@travi/github-scaffolder';
 
+export function javascriptScaffolderFactory(decisions) {
+  return options => scaffoldJavaScript({
+    ...options,
+    configs: {
+      eslint: {scope: '@form8ion'},
+      remark: '@form8ion/remark-lint-preset',
+      babelPreset: {name: '@form8ion', packageName: '@form8ion/babel-preset'},
+      commitlint: {name: '@form8ion', packageName: '@form8ion/commitlint-config'}
+    },
+    overrides: {npmAccount: 'form8ion'},
+    ciServices: {'GitHub Actions': {scaffolder: scaffoldGitHubActionsCi, public: true}},
+    applicationTypes: {},
+    packageTypes: {},
+    unitTestFrameworks: {mocha: {scaffolder: scaffoldMocha}},
+    decisions
+  });
 }
 
-export function githubPromptFactory() {
-
+export function githubPromptFactory(decisions) {
+  return () => prompt({account: 'pragmatic-divops', decisions});
 }
