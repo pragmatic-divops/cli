2813ad8d0df1c55490ea786b60e949ed1ddbc2a9
Matt Travi <programmer@travi.org>
2020-08-27 23:17:22 -0500

    build(rollup): added the node-resolve plugin
    to enable using index files as manifest files

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import autoExternal from 'rollup-plugin-auto-external';
 import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal(), json()],
+  plugins: [autoExternal(), json(), nodeResolve({mainFields: ['module']})],
   output: [{file: 'bin/pragmatic-divops.js', format: 'cjs', sourcemap: true, banner: '#!/usr/bin/env node'}]
 };
