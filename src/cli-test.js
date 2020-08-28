a5fae71dd8a104fde7b4be7b59aea521a46ccd8e
Matt Travi <programmer@travi.org>
2020-08-27 23:07:04 -0500

    feat(scaffold): wired up the command

---
+++ src/cli-test.js
@@ -0,0 +1,26 @@
+import any from '@travi/any';
+import {assert} from 'chai';
+import sinon from 'sinon';
+import * as scaffoldCommand from './commands/scaffold';
+import cli from './cli';
+
+suite('cli', () => {
+  test('that commands are registered', () => {
+    const argv = any.simpleObject();
+    const scriptName = sinon.stub();
+    const usage = sinon.stub();
+    const command = sinon.stub();
+    const demandCommand = sinon.stub();
+    const help = sinon.stub();
+    const alias = sinon.stub();
+    scriptName.withArgs('pragmatic-divops').returns({usage});
+    usage.withArgs('Usage: $0 <cmd> [args]').returns({command});
+    command.withArgs(scaffoldCommand).returns({demandCommand});
+    demandCommand.withArgs(1, 'You need at least one command before moving on').returns({help});
+    help.withArgs('h').returns({alias});
+    alias.withArgs('h', 'help').returns({argv});
+    const yargs = {scriptName};
+
+    assert.equal(cli(yargs), argv);
+  });
+});
