e52904de96021693852843ccc7c2f5a7270c8ed7
Matt Travi <programmer@travi.org>
2020-08-28 00:50:26 -0500

    fix(bundle): made the binary executable and made source-map support and external dependency

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,16 @@
 import autoExternal from 'rollup-plugin-auto-external';
 import json from 'rollup-plugin-json';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import executable from 'rollup-plugin-executable';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal(), json(), nodeResolve({mainFields: ['module']})],
+  plugins: [
+    autoExternal(),
+    json(),
+    executable(),
+    nodeResolve({mainFields: ['module']})
+  ],
+  external: ['source-map-support/register'],
   output: [{file: 'bin/pragmatic-divops.js', format: 'cjs', sourcemap: true, banner: '#!/usr/bin/env node'}]
 };
