c85c9d84b457bcd334f6d20a5879efab75b27b24
Matt Travi <programmer@travi.org>
2024-10-20 11:24:58 -0500

    test(unit): restore mocks globally

--- src/commands/lift/command.test.js
+++ src/commands/lift/command.test.js
@@ -1,14 +1,14 @@
 import * as lifter from '@form8ion/lift';
 import {
   lift as liftRenovate,
-  test as renovatePredicate,
-  scaffold as scaffoldRenovate
+  scaffold as scaffoldRenovate,
+  test as renovatePredicate
 } from '@form8ion/renovate-scaffolder';
 import {scaffold as scaffoldCucumber} from '@form8ion/cucumber-scaffolder';
 import * as githubWorkflowsPlugin from '@form8ion/github-actions-node-ci';
 import * as githubPlugin from '@form8ion/github';
 
-import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
+import {describe, expect, it, vi} from 'vitest';
 import any from '@travi/any';
 import {when} from 'jest-when';
 
@@ -16,17 +16,11 @@ import {javascriptPluginFactory} from '../../common/enhanced-plugins.js';
 import * as enhancedLifters from './enhanced-lifters.js';
 import {command, describe as commandDescription, handler} from './index.js';
 
+vi.mock('@form8ion/lift');
+vi.mock('./enhanced-lifters.js');
+vi.mock('../../common/enhanced-plugins.js');
+
 describe('lift command', () => {
-  beforeEach(() => {
-    vi.mock('@form8ion/lift');
-    vi.mock('./enhanced-lifters.js');
-    vi.mock('../../common/enhanced-plugins.js');
-  });
-
-  afterEach(() => {
-    vi.clearAllMocks();
-  });
-
   it('should define the lift command', async () => {
     const liftingResults = any.simpleObject();
     const decisions = any.simpleObject();
